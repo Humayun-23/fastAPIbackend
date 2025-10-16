7a33fb8bbff80ffd64cb6c4ada27967bd4942b3b
Humayun-23 <humayunroshid2@gmail.com>
2025-10-16 22:52:42 +0530

    updated directory structure

--- app/main.py
+++ app/main.py
@@ -3,20 +3,15 @@ from fastapi import FastAPI
 from .database import engine, get_db
 from .routers import post,user,auth
 from . import models
+from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
-
-
 
 
