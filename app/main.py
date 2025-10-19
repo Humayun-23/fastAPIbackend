b8510c72c6fe2263a1ce991934426c285207ca18
Humayun-23 <humayunroshid2@gmail.com>
2025-10-19 22:16:43 +0530

    fixed voting on non existing post fixed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from dbm import error
 from fastapi import FastAPI
 from .database import engine, get_db
-from .routers import post,user,auth
+from .routers import post,user,auth, vote
 from . import models
 from .config import settings
 
@@ -13,5 +13,6 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 
